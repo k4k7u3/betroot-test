272495dc135c02b3232e0917607c7975544fef7a
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-04-10 21:30:14 +0300

    task3 lesson1

---
+++ lesson1_test3.py
@@ -0,0 +1,49 @@
+# Вариант 1 в лоб и очень примитивно т.е. печатаем тупо принтами
+print('method 1:')
+print('#'*9, end='\n')
+print('#','#', sep=' '*7, end='\n')
+print('#','#', sep=' '*7, end='\n')
+print('#','#', sep=' '*7, end='\n')
+print('#'*9, end='\n'*2)
+
+print('#','#', sep=' '*7, end='\n')
+print('#','#', sep=' '*7, end='\n')
+print('#'*9, end='\n')
+print('#','#', sep=' '*7, end='\n')
+print('#','#', sep=' '*7, end='\n'*2)
+
+#Вариант второй програма становится красивее ( имхо =)  
+print('method 2:')
+b = '#'*9
+a = '#' + ' '*7 + '#'
+print(b)
+print(a)
+print(a)
+print(a)
+print(b + '\n')
+
+print(a)
+print(a)
+print(b)
+print(a)
+print(a + '\n')
+
+# Вариант с использованием цикла но с одним принтом - потому что во втором примере не получается использовать print(a * 3)
+print('method 3:')
+text = '#' + ' '*7 + '#'
+no_change = [1,2,3]
+no_change1 = 2
+
+for i in range(5):
+    if i not in no_change:
+        print(text.replace(' ', '#'))
+    else:
+        print(text)
+
+print()
+
+for i in range(5):
+    if i == no_change1:
+        print(text.replace(' ', '#'))
+    else:
+        print(text)
