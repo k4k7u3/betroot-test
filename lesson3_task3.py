209ca63a9c6502471c965e5332e617df0cf77daf
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-04-16 18:44:18 +0300

    lesson4_only for me

--- lesson3_task3.py
+++ lesson3_task3.py
@@ -1,8 +1,11 @@
-name = "sasha"
+# name = "sasha"
 
-input_name = input("Введите свое имя: ")
+# input_name = input("Введите свое имя: ")
 
-if input_name.lower() == name:
-	print("Имя совпадает")
-else:
-	print("Имя не совпадает")
+# if input_name.lower() == name:
+# 	print("Имя совпадает")
+# else:
+# 	print("Имя не совпадает")
+n = 1
+while n > 0:
+	print("rere")
---
+++ lesson4_task3_only_for_me.py
@@ -0,0 +1,9 @@
+import random
+
+s = input('Input something: ')
+x = 0
+st = list(s)
+while x != 5:
+	random.shuffle(st)
+	print(st)
+	x += 1
