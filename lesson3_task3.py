b5a9fa08eea3872af71ef6a2c1733773d66edba9
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-04-20 13:53:40 +0300

    Update lesson3_task3.py
    Разкоментировал

--- lesson3_task3.py
+++ lesson3_task3.py
@@ -1,11 +1,8 @@
-# name = "sasha"
+name = "sasha"
 
-# input_name = input("Введите свое имя: ")
+input_name = input("Введите свое имя: ")
 
-# if input_name.lower() == name:
-# 	print("Имя совпадает")
-# else:
-# 	print("Имя не совпадает")
-n = 1
-while n > 0:
-	print("rere")
+if input_name.lower() == name:
+	print("Имя совпадает")
+else:
+	print("Имя не совпадает")
