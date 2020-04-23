8cf7ac0f50a5bcbe4eed6f42148bfc2a8e671d5e
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-04-23 22:01:55 +0300

    update  task

--- lesson4/lesson4_task1.py
+++ lesson4/lesson4_task1.py
@@ -1,16 +1,13 @@
 import random
 
 x = []
-i = 10
-while i != 0:
-	x.append(random.randint(0, 100))
-	i -= 1
-print('Вот список из случайных чисел: ', x)
-big = x[0]
+i = 0
+big = 0
 while i != 10:
-	if big > x[i]:
-		pass
-	else:
+	x.append(random.randint(0, 100))
+	if x[i] > big:
 		big = x[i]
 	i += 1
+
+print('Вот список из случайных чисел: ', x)
 print('Наибольшее число из списка - ', big)
