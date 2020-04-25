4a984828e57dd3d28dd5985a843297e6ec0b8a69
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-04-25 09:47:10 +0300

    lesson4_task3 only one str

--- lesson4/lesson4_task2.py
+++ lesson4/lesson4_task2.py
@@ -3,7 +3,6 @@ import random
 list_1 = []
 list_2 = []
 list_3 = []
-
 i = 0
 while i!= 10:
 	list_1.append(random.randint(1, 10))
@@ -17,4 +16,3 @@ while i!= 10:
 print(list_1)
 print(list_2)
 print(list_3)
-
--- lesson4/lesson4_task3.py
+++ lesson4/lesson4_task3.py
@@ -2,11 +2,5 @@ import random
 
 x = []
 z = []
-i = 0
-while i != 100:
-	x.append(i+1)
-	if x[i] % 7 == 0 and x[i] % 5 != 0:
-		z.append(x[i])
-	i += 1
+x = [x for x in range(100) if x % 7 == 0 and x % 5]
 print(x)
-print(z)
