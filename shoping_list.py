d8f44c34d45ade1c4ad32d407f8ef048f73d61b4
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-05-11 16:17:04 +0300

    v 2.0
    Исправил про то, что ты говорил , что бы можно было вводить покупку (Бутылка вина) .
    ВЫнес пару вводов в функции, честно я пока не понимаю, что еще можно вынести в функции

--- shoping_list.py
+++ shoping_list.py
@@ -1,21 +1,27 @@
 all_purchase = {'Other' : []}
 category = [all_purchase]
 
+def input_mode(input_text):
+    x = input(input_text)
+    x = x.capitalize().replace(' ', '')
+    return x
+
+def input_mode2(input_text2):
+    y = input(input_text2)
+    y = y.capitalize().strip()
+    return y
 
 while True:
-    operation = input("Hey! Choose mode: (1 - control category; 2 - control purchase; 'exit' - exit): ")
-    operation = operation.capitalize().replace(' ', '')
+    operation = input_mode("Hey! Choose mode: (1 - control category; 2 - control purchase; 'exit' - exit): ")
     if operation == '1':
         while True:
             print("Your list of category:", category)
-            category_menu = input("What do you want to do? (1 - choose category; 2 - add category; 3 - delete category; 'exit' - exit): ")
-            category_menu = category_menu.capitalize().replace(' ', '')
+            category_menu = input_mode("What do you want to do? (1 - choose category; 2 - add category; 3 - delete category; 'exit' - exit): ")
             if category_menu == 'Exit':
                 break
             elif category_menu == '1':
                 while True:
-                    name_category = input(f"This is your category {category}. Which do you choose? ('other',.., 'exit' - exit): ")
-                    name_category = name_category.capitalize().replace(' ', '')
+                    name_category = input_mode2(f"This is your category {category}. Which do you choose? ('other',.., 'exit' - exit) : ")
                     if name_category == 'Exit':
                         break
                     elif name_category not in all_purchase:
@@ -25,8 +31,7 @@ while True:
                         temporary_purchase = all_purchase.get(name_category)
                         while True:
                             print(temporary_purchase)
-                            operation_with_purchase = input("What do you want to do with this purchase? (1 - add new purchase; 2 - delete some purchase; 3 - delete all purchase; 'exit' - exit): ")
-                            operation_with_purchase = operation_with_purchase.capitalize().replace(' ', '')
+                            operation_with_purchase = input_mode("What do you want to do with this purchase? (1 - add new purchase; 2 - delete some purchase; 3 - delete all purchase; 'exit' - exit): ")
                             if operation_with_purchase == 'Exit':
                                 break
                             elif operation_with_purchase == '1':
@@ -35,12 +40,12 @@ while True:
                                     continue
                                 else:
                                     name_new_purchase = input("Input new purchase: ")
-                                    name_new_purchase = name_new_purchase.capitalize().replace(' ', '')
+                                    name_new_purchase = name_new_purchase.strip()
                                     temporary_purchase.append(name_new_purchase)
                                     continue
                             elif operation_with_purchase == '2':
                                 del_purchase = input("Input name of purchase which you want delete: ")
-                                del_purchase = del_purchase.capitalize().replace(' ', '')
+                                del_purchase = del_purchase.strip()
                                 if del_purchase not in temporary_purchase:
                                     print("We don't have this purchase! Try again")
                                     continue
@@ -54,14 +59,12 @@ while True:
                                 print("We don't support this operation. Try again")
                                 continue
             elif category_menu == '2':
-                name_new_category = input("Input a name for the new category: ")
-                name_new_category = name_new_category.capitalize().replace(' ', '')
+                name_new_category = input_mode2("Input a name for the new category: ")
                 all_purchase[name_new_category] = []
                 continue
             elif category_menu == '3':
                 while True:
-                    name_del_category = input("Input a name for the new category: ")
-                    name_del_category = name_del_category.capitalize().replace(' ', '')
+                    name_del_category = input_mode2("Input a name for the new category: ")
                     if name_del_category not in all_purchase:
                         print("We don't have this category! Try again")
                         continue
@@ -82,12 +85,14 @@ while True:
                 continue
     elif operation == '2':
         temporary_purchase = all_purchase.get('Other')
+        if temporary_purchase == None:
+            all_purchase["Other"] = []
+            temporary_purchase = all_purchase.get('Other')
         print(f'This is your list of all_purchase: {temporary_purchase}')
         while True:
             print(temporary_purchase)
-            operation_with_purchase = input(
+            operation_with_purchase = input_mode(
                 "What do you want to do with this purchase? (1 - add new purchase; 2 - delete some purchase; 3 - delete all purchase; 'exit' - exit): ")
-            operation_with_purchase = operation_with_purchase.capitalize().replace(' ', '')
             if operation_with_purchase == 'Exit':
                 break
             elif operation_with_purchase == '1':
@@ -96,12 +101,12 @@ while True:
                     continue
                 else:
                     name_new_purchase = input("Input new purchase: ")
-                    name_new_purchase = name_new_purchase.capitalize().replace(' ', '')
+                    name_new_purchase = name_new_purchase.strip()
                     temporary_purchase.append(name_new_purchase)
                     continue
             elif operation_with_purchase == '2':
                 del_purchase = input("Input name of purchase which you want delete: ")
-                del_purchase = del_purchase.capitalize().replace(' ', '')
+                del_purchase = del_purchase.strip()
                 if del_purchase not in temporary_purchase:
                     print("We don't have this purchase! Try again")
                     continue
