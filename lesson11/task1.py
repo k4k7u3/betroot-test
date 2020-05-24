00d4b548c1a3ddf6f5cc3c1d208c8a954a6ec2bd
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-05-24 13:22:24 +0300

    update task1

--- lesson11/task1.py
+++ lesson11/task1.py
@@ -5,10 +5,24 @@ class Person:
 
 
 class Student(Person):
+    marks_per_semesters = {}
+    final_marks = {}
+    lessons = None
+    number_of_class = None
+
+    def get_final_mark(self):
+        return self.final_marks
     pass
 
 
 class Teacher(Person):
+    wages = None
+    experience = None
+    dress_code = None
+    taxes = None
+    category = None
 
+    def calculate_wages(self):
+        return self.wages + (self.experience * 250) + (self.category * 200) - self.taxes  # for example
     pass
 
