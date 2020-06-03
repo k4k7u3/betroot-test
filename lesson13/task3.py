0f61016d11e872c73db2e946f66b90b391437616
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-06-03 21:36:55 +0300

    Update task3.py
    update task3

--- lesson13/task3.py
+++ lesson13/task3.py
@@ -5,24 +5,15 @@ def args_rule(type_, max_length, contains):
     def decor(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            result = ""
-            try:
-                for item in args:
-                    if type(item) != type_:
-                        raise ValueError
-                    elif len(item) > max_length:
-                        raise Exception(f"Len -> '{item}' should be no more than {max_length}")
-                    for i in contains:
-                        if i not in item:
-                            raise Exception(f"'{item}' should include {contains}")
-                result = func(*args, **kwargs)
-                return True, result
-            except ValueError:
-                result = f"Type args '{item}' should be a string"
-                return False, result
-            except Exception as e:
-                result = str(e)
-                return False, result
+            for item in args:
+                if type(item) != type_:
+                    return False, f"Type args '{item}' should be a string"
+                elif len(item) > max_length:
+                    return False, f"Len -> '{item}' should be no more than {max_length}"
+                if any(i not in item for i in contains):
+                    return False, f"'{item}' should include {contains}"
+            result = func(*args, **kwargs)
+            return True, result
         return wrapper
     return decor
 
@@ -31,6 +22,7 @@ def args_rule(type_, max_length, contains):
 def my_func(string_one, string_two):
     return string_one + ". " + string_two
 
+
 str_one = "any"
-str_two = "more than 15 it is a true"
+str_two = "more than y"
 print(my_func(str_one, str_two))
