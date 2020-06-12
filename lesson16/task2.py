3da221b7362874514d2053fee9949b7f52922810
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-06-12 21:24:12 +0300

    Update task2.py

--- lesson16/task2.py
+++ lesson16/task2.py
@@ -3,25 +3,34 @@ def in_range(start, end=0, step=1):
         raise ValueError
     if step == 0:
         raise ValueError("'Step' should not be a zero")
-    elif step > 0:
-        if start > end:
+    if step >= 1:
+        if end == 0:
             while end <= start-1:
                 yield end
-                end += 1
-        else:
-            while start <= end-1:
+                end += step
+        if end < 0 or end > 0:
+            if start < end:
+                while start <= end-1:
+                    yield start
+                    start += step
+            if start > end:
+                return []
+    if step < 0:
+        if end > start:
+            return []
+        if start > end:
+            while start >= end+1:
                 yield start
                 start += step
-    else:
-        while start >= end+1:
-            yield start
-            start += step
 
-
-x = in_range(10, 5, -1)
+x = in_range(10, 12)
+y = range(10, 12)
+print("x")
 for i in x:
     print(i)
-
+print("y")
+for i in y:
+    print(i)
 
 
 
