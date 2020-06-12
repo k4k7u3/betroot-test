60373ad2a3b71dee1076a4ca35048a4d8dc54dd9
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-06-12 17:35:23 +0300

    update task1

--- lesson16/task1.py
+++ lesson16/task1.py
@@ -4,13 +4,17 @@ from _collections_abc import Iterable
 def with_index(iterable, start=0):
     if not isinstance(iterable, Iterable):
         raise TypeError(f"{iterable} should be iterable")
-    if start >= len(iterable):
-        raise ValueError
-    yield start, iterable[start]
+    for item in iterable:
+      yield start, item
+      start += 1
 
 
 x = [10, 20, 30, 40]
 
-iterable = with_index(x, 1)
+iterable = with_index(x, 50)
+y = enumerate(x, 50)
 print(next(iterable))
+print(next(y))
+print(next(iterable))
+print(next(y))
 
