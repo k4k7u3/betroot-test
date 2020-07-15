ddf023731c92c87bab5fbc3c7e7c9c9f23e50491
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-07-15 20:35:05 +0300

    Update task1_client.py

--- lesson25/task1_client.py
+++ lesson25/task1_client.py
@@ -5,9 +5,9 @@ i = 0
 IP = "127.0.0.1"
 PORT = 65432
 
-message = "Hello UPD"
+message = b"Hello UPD"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-while i < 10:
+for i in range(10):
     sock.sendto(bytes(message, encoding='utf-8'), (IP, PORT))
-    i += 1
+    
