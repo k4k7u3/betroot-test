68af5a9655a68b9b71f338884214901f78d3e789
k4k7u3 <63358547+k4k7u3@users.noreply.github.com>
2020-07-15 22:16:31 +0300

    Update task2_server.py

--- lesson25/task2_server.py
+++ lesson25/task2_server.py
@@ -3,50 +3,46 @@ import socket
 IP = "127.0.0.1"
 PORT = 65432
 
-not_letter = ["!", ".", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", " "]
-dictionary = {
-        1: "a",
-        2: "b",
-        3: "c",
-        4: "d",
-        5: "e",
-        6: "f",
-        7: "g",
-        8: "h",
-        9: "i",
-        10: "j",
-        11: "k",
-        12: "l",
-        13: "m",
-        14: "n",
-        15: "o",
-        16: "p",
-        17: "q",
-        18: "r",
-        19: "s",
-        20: "t",
-        21: "u",
-        22: "v",
-        23: "w",
-        24: "x",
-        25: "y",
-        26: "z"}
-key = 3
+# not_letter = ["!", ".", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", " "]
+# dictionary = {
+#         1: "a",
+#         2: "b",
+#         3: "c",
+#         4: "d",
+#         5: "e",
+#         6: "f",
+#         7: "g",
+#         8: "h",
+#         9: "i",
+#         10: "j",
+#         11: "k",
+#         12: "l",
+#         13: "m",
+#         14: "n",
+#         15: "o",
+#         16: "p",
+#         17: "q",
+#         18: "r",
+#         19: "s",
+#         20: "t",
+#         21: "u",
+#         22: "v",
+#         23: "w",
+#         24: "x",
+#         25: "y",
+#         26: "z"}
+key = 0
 
 
 def cesar(input_str: str) -> str:
     pass_str = ""
-    input_str = input_str.lower()
+    number_of_letter = 0
     for item in input_str:
-        if item in not_letter:
-            pass_str += item
-            continue
-        for j in dictionary:
-            if item == dictionary[j]:
-                number_of_letter = j + key
-                if number_of_letter > 26:
-                    number_of_letter -= 26
-                pass_str += dictionary[number_of_letter]
+        if ord(item) in range(32, 127):
+            number_of_letter = ord(item) + key
+            if number_of_letter > 127:
+                number_of_letter -= (127 - 32)
+        pass_str += chr(number_of_letter)
     return pass_str
 
 
